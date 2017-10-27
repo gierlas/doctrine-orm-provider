7ae0dd8a58b237ba518fed56d1152275300b8ec0
Pawel Gierlasinski <gierlasinski.pawel@gmail.com>
2017-10-27 13:37:12 +0200

    Choice filter functional test

--- Tests/OperatorImplementation/Filter/ChoiceFilterImplementationTest.php
+++ Tests/OperatorImplementation/Filter/ChoiceFilterImplementationTest.php
@@ -4,8 +4,11 @@ namespace Kora\DataProvider\Doctrine\Orm\Tests\OperatorImplementation\Filter;
 
 use Doctrine\ORM\Query\Expr;
 use Doctrine\ORM\QueryBuilder;
+use Kora\DataProvider\DataProviderOperatorsSetup;
 use Kora\DataProvider\Doctrine\Orm\OperatorImplementation\Filter\ChoiceFilterImplementation;
 use Kora\DataProvider\Doctrine\Orm\OrmDataProvider;
+use Kora\DataProvider\Doctrine\Orm\OrmImplementationList;
+use Kora\DataProvider\Doctrine\Orm\Tests\AbstractDoctrineTest;
 use Kora\DataProvider\Mapper;
 use Kora\DataProvider\OperatorDefinition\Filter\ChoiceFilter\ChoiceProvider\ArrayProvider;
 use Kora\DataProvider\OperatorDefinition\Filter\ChoiceFilterDefinition;
@@ -17,9 +20,50 @@ use Mockery as m;
  * Class ChoiceFilterImplementationTest
  * @author Paweł Gierlasiński <pawel@mediamonks.com>
  */
-class ChoiceFilterImplementationTest extends TestCase
+class ChoiceFilterImplementationTest extends AbstractDoctrineTest
 {
 	use m\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
+	/**
+	 * @dataProvider resultProvider
+	 * @param $term
+	 * @param $choicesProvider
+	 * @param $isMulti
+	 * @param $expectedCount
+	 */
+	public function testResult($term, $choicesProvider, $isMulti, $expectedCount)
+	{
+		$em = $this->getPropagatedEM();
+		$qb = $em->createQueryBuilder()
+			->select('f')
+			->from('Test:Foo', 'f');
+
+		$setup = new DataProviderOperatorsSetup();
+		$setup
+			->addFilter(new ChoiceFilterDefinition('nbValue', $choicesProvider, $isMulti, FILTER_VALIDATE_INT));
+
+		$setup->setData([
+			'nbValue' => $term
+		]);
+
+		$ormDataProvider = new OrmDataProvider(new OrmImplementationList(), clone $qb, new Mapper());
+		$data = $ormDataProvider->fetchData($setup);
+
+
+		$this->assertEquals($expectedCount, $data->getNbAll());
+		$this->assertCount($expectedCount, $data->getResults());
+	}
+
+	public function resultProvider()
+	{
+		return [
+			[1, new ArrayProvider([1, 2, 3, 4]), false, 2],
+			[2, new ArrayProvider([1, 2, 3, 4]), false, 1],
+			[[1, 2], new ArrayProvider([1, 2, 3, 4]), true, 3],
+			[4, new ArrayProvider([1, 2, 3, 4]), false, 0],
+			[[1, 2, 4], new ArrayProvider([1, 2, 3, 4]), true, 3]
+		];
+	}
 
 	public function testNotMulti()
 	{
